9141200f7ddeed4b396179a91fe880e816b85183
Davi R <davirscunha@gmail.com>
2023-07-17 17:20:17 -0300

    implementação angular -> tela para criar cursos

--- crud-angular/src/app/courses/course-form/course-form.component.spec.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CourseFormComponent } from './course-form.component';
 
 describe('CourseFormComponent', () => {
--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrls: ['./course-form.component.scss']
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit{
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null],
+      price: [null]
+    });
+  }
+
+  ngOnInit(): void {
+      //nothing atm
+  }
+
+  onSubmit(){
+    this.router.navigate([''], {relativeTo: this.route});
+  }
+
+
+
+  onCancel(){
+    this.router.navigate([''], {relativeTo: this.route});
+  }
 
 }
